d13e977fe1753ac4789e2095e458a70b76d0e89c
RehanAziz22 <rehanaziz284@gmail.com>
2024-12-11 11:17:46 +0500

    cart

--- Controllers/ProductController.js
+++ Controllers/ProductController.js
@@ -10,12 +10,56 @@ const productController = {
                 data: product,
                 status: true
             })
-        }else{
+        } else {
             res.json({
                 message: "No products found",
                 status: false
             })
         }
+    },
+    getSingleProduct: async (req, res) => {
+        const { id } = req.params
+
+        const product = await ProductModel.findById(id)
+
+        if (!product) {
+            res.json({
+                message: "Product not found",
+                status: false
+            })
+        } else {
+            res.json({
+                message: "Product found",
+                data: product,
+                status: true
+            })
+        }
+    },
+
+    // Add a review to a product
+    addReview: async (req, res) => {
+  
+            const { id } = req.params;
+            const { reviewerName, reviewerEmail, rating, comment } = req.body;
+            console.log(req.body);
+
+            const product = await ProductModel.findById(id);
+            if (!product) {
+                return res.status(404).json({ message: "Product not found" });
+            }
+
+            const newReview = {
+                reviewerName,
+                reviewerEmail,
+                rating,
+                comment,
+                date: new Date(),
+            };
+
+            product.reviews.push(newReview);
+            await product.save();
+
+            res.status(200).json({ message: "Review added successfully", review: newReview });
     }
 }
 
--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -3,11 +3,14 @@ const productController = require("../Controllers/ProductController")
 
 const router = express.Router()
 
-// http://localhost:3000/api/user/id
-// router.get('/:id', userController.getSingleUser)
+// http://localhost:3000/api/product/id
+router.get('/:id', productController.getSingleProduct)
 
 // http://localhost:3000/api/user/all
 router.get('/', productController.getAllProducts)
+
+// http://localhost:3000/api/product/id/reviews
+router.post("/api/product/:id/reviews",productController.addReview)
 
 // http://localhost:3000/api/user/signup
 // router.post('/signup', userController.signupUser)
